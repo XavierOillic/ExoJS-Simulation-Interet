e7d1e48f9234ef6e1dc616b5aef466e6efd40eb1
XavierOillic <138000000+XavierOillic@users.noreply.github.com>
2023-10-25 00:10:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //deposit = Number(document.getElementById('deposit_input).value);
+// LE PARAMETRE EVENT surveilles les EVENEMENTS et le ONSUBMIT dans le HtML en est UN. IL Y EST AUSSI ENTRE ()
 
+function CalculProfit(event) { // CE PARAMETRE dans le fonction : les autres sont donnés par l'input, et leur assigne cette valeur avec QUERYSELECTOR.
+    event.preventDefault()    //permet de prévenir les actions par défaut du FORM, en l'occurence : recharger sans cesse les pages et appeler des URL.
 
-function CalculProfit(event) {
-    event.preventDefault()    //permet de prévenir les actions par défaut du FORM, en l'occurence.
-    const deposit = Number(document.querySelector('#deposit_input').value); // je recupère, et je stocke dans deposit l'iD de l'INPUT.
-    const rate = Number(document.querySelector('#rate_input').value);
+    const deposit = Number(document.querySelector('#deposit_input').value); // je recupère, et je stocke dans "deposit" la VALEUR de l'iD de l'INPUT.
+    const rate = Number(document.querySelector('#rate_input').value);       // idem pour les suivants.
     const threshold = Number(document.querySelector('#threshold_input').value);
     
     let nbAnnee = 0;                                  // J'initialise mon nb d'année à 0.
@@ -17,7 +18,8 @@ function CalculProfit(event) {
 
    return document.getElementById('resultat_ouput').innerHTML = `${nbAnnee} ANS`;
 
-    // return nbAnnee;                        // Je retourne le nombre d'année: LE RETURN STOPPE LA FONCTION !.
+// Je retourne NBANNEE : LE RETURN STOPPE LA FONCTION !
+// donc je vais chercher l'endroit du html ou je dois l'afficher, je le prend et je lui met avec INNERHTML le valeur de la var NBANNEE
 };
 
 
